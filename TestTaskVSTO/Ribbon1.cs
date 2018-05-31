2d5941587740d4456c0d8738e0454527ba3f65f0
flame11st <flame11.st@gmail.com>
2018-05-31 22:38:36 +0300

    Add project

---
+++ TestTaskVSTO/Ribbon1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using PowerPoint = Microsoft.Office.Interop.PowerPoint;
+using Office = Microsoft.Office.Core;
+
+namespace TestTaskVSTO
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, RibbonControlEventArgs e)
+        {
+            PowerPoint.Application thisApp = Globals.ThisAddIn.Application;
+            PowerPoint.SlideRange slide = thisApp.ActiveWindow.Selection
+                .SlideRange;
+            var slideWidth = slide.CustomLayout.Width;
+            
+            foreach (PowerPoint.Shape shape in slide.Shapes)
+            {
+                if(shape.Type == Office.MsoShapeType.msoTextBox)
+                {
+                    if((shape.Width+shape.Left+10) <= slideWidth)
+                    {
+                        shape.IncrementLeft(10);
+                    }
+                    shape.TextEffect.FontBold = Office.MsoTriState.msoTrue;
+                    shape.TextFrame.TextRange.Font.Color.RGB = 255;
+                }
+            }
+        }
+    }
+}
